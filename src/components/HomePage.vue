7d659f1aa4f0931330e03f66454c7438d27ef4f1
theringsofsaturn <emiliankasemi@gmail.com>
2023-05-14 19:03:53 +0200

    added json server and api calls for special offers and popular items

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -12,14 +12,28 @@
       <v-row>
         <v-col>
           <h2>Special Offers</h2>
-          <!-- Will make it a carousel or a grid component -->
           <v-carousel>
-            <!-- Loop through special offers -->
             <v-carousel-item
               v-for="(offer, index) in specialOffers"
               :key="index"
             >
-              <!-- Display offer details -->
+              <v-card>
+                <div class="d-flex justify-center">
+                  <v-img
+                    :src="offer.image"
+                    alt="offer.name"
+                    height="400"
+                    width="600"
+                    contain
+                    class="align-center"
+                  ></v-img>
+                </div>
+                <v-card-title>{{ offer.name }}</v-card-title>
+                <v-card-text>Price: ${{ offer.price }}</v-card-text>
+                <v-card-text
+                  >Discount Price: ${{ offer.discountPrice }}</v-card-text
+                >
+              </v-card>
             </v-carousel-item>
           </v-carousel>
         </v-col>
@@ -28,13 +42,31 @@
       <v-row>
         <v-col>
           <h2>Our Popular Items</h2>
-          <!-- Will make it a list or a grid -->
-          <v-list>
-            <!-- Loop through popular items -->
-            <v-list-item v-for="(item, index) in popularItems" :key="index">
-              <!-- Display item details -->
-            </v-list-item>
-          </v-list>
+          <v-row>
+            <v-col
+              cols="12"
+              sm="6"
+              md="4"
+              lg="3"
+              v-for="(item, index) in popularItems"
+              :key="index"
+            >
+              <v-card>
+                <div class="d-flex justify-center">
+                  <v-img
+                    :src="item.image"
+                    alt="item.name"
+                    height="200"
+                    width="300"
+                    contain
+                    class="align-center pa-2"
+                  ></v-img>
+                </div>
+                <v-card-title>{{ item.name }}</v-card-title>
+                <v-card-subtitle>Price: ${{ item.price }}</v-card-subtitle>
+              </v-card>
+            </v-col>
+          </v-row>
         </v-col>
       </v-row>
     </v-container>
@@ -42,19 +74,34 @@
 </template>
 
 <script>
+import axios from 'axios';
+
 export default {
   name: 'HomePage',
   data() {
     return {
-      // Will replace these with actual data
       specialOffers: [],
       popularItems: [],
     };
   },
-
-  // Will fetch the data in created so that it is available when the component is rendered
   created() {
-    // Fetch special offers and popular items
+    axios
+      .get('http://localhost:3000/specialOffers')
+      .then((response) => {
+        this.specialOffers = response.data;
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+
+    axios
+      .get('http://localhost:3000/popularItems')
+      .then((response) => {
+        this.popularItems = response.data;
+      })
+      .catch((error) => {
+        console.log(error);
+      });
   },
 };
 </script>
