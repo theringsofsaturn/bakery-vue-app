3985c71de6e6ddba02dca4cedc022ad5c3b41f43
theringsofsaturn <emiliankasemi@gmail.com>
2023-05-16 03:18:22 +0200

    Small bug displaying twice all products

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,19 @@
 <template>
   <v-app>
     <TopNavBar></TopNavBar>
-    <HomePage></HomePage>
-    <AppFooter></AppFooter>
     <router-view />
+    <AppFooter></AppFooter>
   </v-app>
 </template>
 
 <script>
 import TopNavBar from './components/TopNavBar.vue';
 import AppFooter from './components/AppFooter.vue';
-import HomePage from './components/HomePage.vue';
 
 export default {
   components: {
     TopNavBar,
     AppFooter,
-    HomePage,
   },
 };
 </script>
