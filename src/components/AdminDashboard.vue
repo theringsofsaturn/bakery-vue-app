c5a0dd3dc4423a25342548b7b04ed1f49279d901
theringsofsaturn <emiliankasemi@gmail.com>
2023-05-16 17:38:33 +0200

    Admin dashboard placeholder component and checheck for authentication

---
+++ src/components/AdminDashboard.vue
@@ -0,0 +1,13 @@
+<template>
+  <v-app>
+    <v-main>
+      <v-container>Hello world</v-container>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+export default {
+  name: 'AdminDashboard',
+};
+</script>
--- src/router/index.js
+++ src/router/index.js
@@ -5,6 +5,7 @@ import HomePage from '../components/HomePage.vue';
 import ProductList from '../components/ProductList.vue';
 import ProductDetail from '../components/ProductDetail.vue';
 import LoginPage from '../components/LoginPage.vue';
+import AdminDashboard from '../components/AdminDashboard.vue';
 
 Vue.use(VueRouter);
 
@@ -29,12 +30,41 @@ const routes = [
     name: 'login',
     component: LoginPage,
   },
+  {
+    path: '/dashboard',
+    name: 'dashboard',
+    component: AdminDashboard,
+    meta: {
+      requiresAuth: true,
+    },
+  },
 ];
 
 const router = new VueRouter({
   mode: 'history',
   base: process.env.BASE_URL,
   routes,
+});
+
+// check if a route requires authentication by looking at the requiresAuth meta field. If the route does require authentication, it checks if the user is logged in. This check is done using a simple flag in local storage in this example, but in a real application I would probably want to use a more secure method of storing the user's authentication status.
+router.beforeEach((to, from, next) => {
+  if (to.matched.some((record) => record.meta.requiresAuth)) {
+    // This route requires auth, check if the user is logged in
+    // For now, let's just use a simple flag in local storage
+    if (!localStorage.getItem('loggedIn')) {
+      // User is not logged in, redirect to login page
+      next({
+        path: '/login',
+        query: { redirect: to.fullPath },
+      });
+    } else {
+      // User is logged in, allow access
+      next();
+    }
+  } else {
+    // This route doesn't require auth, always allow access
+    next();
+  }
 });
 
 export default router;
