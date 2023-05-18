b9924b01079c86f782a686d46cf74ab4746d2d38
theringsofsaturn <emiliankasemi@gmail.com>
2023-05-18 03:22:21 +0200

    setup mongodb and added connection string

--- backend/app.js
+++ backend/app.js
@@ -1,10 +1,19 @@
 import express from 'express';
+import mongoose from 'mongoose';
 const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
+
+mongoose
+  .connect('mongodb+srv://ishmael:dostojevski@cluster0.oqgfe.mongodb.net/', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log('MongoDB connected...'))
+  .catch((err) => console.log(err));
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
