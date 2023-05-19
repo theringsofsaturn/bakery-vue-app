11d2fe78516ed0555e606abf000645ce4387bb73
theringsofsaturn <emiliankasemi@gmail.com>
2023-05-19 02:54:19 +0200

    configure multer in express app

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import cors from 'cors';
+import multer from 'multer';
 import authRouter from './routes/auth.js';
 import productRouter from './routes/products.js';
 
@@ -12,6 +13,32 @@ app.use(cors());
 
 app.use('/', authRouter);
 app.use('/', productRouter);
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, './uploads/');
+  },
+  filename: (req, file, cb) => {
+    cb(null, new Date().toISOString().replace(/:/g, '-') + file.originalname);
+  },
+});
+
+const fileFilter = (req, file, cb) => {
+  // Accept only images
+  if (file.mimetype.startsWith('image/')) {
+    cb(null, true);
+  } else {
+    cb(null, false);
+  }
+};
+
+const upload = multer({
+  storage: storage,
+  limits: {
+    fileSize: 1024 * 1024 * 5, // Allow images of up to 5MB
+  },
+  fileFilter: fileFilter,
+});
 
 mongoose
   .connect('mongodb+srv://ishmael:dostojevski@cluster0.oqgfe.mongodb.net/', {
